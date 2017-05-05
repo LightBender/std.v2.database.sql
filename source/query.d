f65de939335ca4bee59e1d9831f194394760e535
Adam Wilson <adamw@ellipticbit.com>
2017-05-05 15:24:20 +0200

    SqlTable works using Variant.

--- source/query.d
+++ source/query.d
@@ -6,12 +6,8 @@ import std.variant;
 import std.uuid;
 
 import std.experimental.database.sql.connection;
+import std.experimental.database.sql.reader;
 import std.experimental.database.sql.table;
-
-public alias SqlPrimitive = Algebraic!(bool, byte, ubyte, short, ushort, int, uint, long, ulong,
-        // cent, ucent, -- Compiler error when included
-		float, double, real, ifloat, idouble, ireal, cfloat, cdouble, creal, char, wchar, dchar,
-		ubyte[], string, wstring, dstring, UUID, Date, DateTime, TimeOfDay, Duration, SqlTable, Object);
 
 public enum SqlParameterDirection
 {
@@ -23,7 +19,7 @@ public enum SqlParameterDirection
 
 public struct SqlParameter
 {
-    Nullable!SqlPrimitive value;
+    Nullable!Variant value;
     bool nullable;
     int length;
     ubyte precision;
@@ -44,5 +40,5 @@ public interface SqlExecutor
 {
     void QueryNoResult(SqlQuery query);
     SqlReader QueryReader(SqlQuery query);
-    SqlTable QueryTable(SqlQuery query);
+    SqlTableBase QueryTable(SqlQuery query);
 }
--- source/table.d
+++ source/table.d
@@ -7,98 +7,49 @@ import std.typecons;
 import std.variant;
 import std.uuid;
 
-import std.experimental.database.sql.query;
-
-public final class SqlTable
+public abstract class SqlTableBase
 {
-	private Nullable!SqlPrimitive[][] table;
+	protected Nullable!Variant[][] table;
 
-	public this(T...)(T columnTypes)
-		if(allSatisfy!(columnTypes, bool, byte, ubyte, short, ushort, int, uint, long, ulong,// cent, ucent, -- Compiler error when included
-			float, double, real, ifloat, idouble, ireal, cfloat, cdouble, creal, char, wchar, dchar,
-			ubyte[], string, wstring, dstring, UUID, Date, DateTime, TimeOfDay, Duration, SqlTable, Object))
+	public Nullable!T getField(T)(uint row, uint column)
 	{
-		table.reserve(columnTypes.length);
-
-		for(int i = 0; i < columnTypes.length; i++)
-		{
-			if(is(columnTypes[i] == bool)) table[i] = new Nullable!(Algebraic!bool)[];
-			if(is(columnTypes[i] == byte)) table[i] = new Nullable!(Algebraic!byte)[];
-			if(is(columnTypes[i] == ubyte)) table[i] = new Nullable!(Algebraic!ubyte)[];
-			if(is(columnTypes[i] == short)) table[i] = new Nullable!(Algebraic!short)[];
-			if(is(columnTypes[i] == ushort)) table[i] = new Nullable!(Algebraic!ushort)[];
-			if(is(columnTypes[i] == int)) table[i] = new Nullable!(Algebraic!int)[];
-			if(is(columnTypes[i] == uint)) table[i] = new Nullable!(Algebraic!uint)[];
-			if(is(columnTypes[i] == long)) table[i] = new Nullable!(Algebraic!long)[];
-			if(is(columnTypes[i] == ulong)) table[i] = new Nullable!(Algebraic!ulong)[];
-			if(is(columnTypes[i] == float)) table[i] = new Nullable!(Algebraic!float)[];
-			if(is(columnTypes[i] == double)) table[i] = new Nullable!(Algebraic!double)[];
-			if(is(columnTypes[i] == real)) table[i] = new Nullable!(Algebraic!real)[];
-			if(is(columnTypes[i] == ifloat)) table[i] = new Nullable!(Algebraic!ifloat)[];
-			if(is(columnTypes[i] == idouble)) table[i] = new Nullable!(Algebraic!idouble)[];
-			if(is(columnTypes[i] == ireal)) table[i] = new Nullable!(Algebraic!ireal)[];
-			if(is(columnTypes[i] == cfloat)) table[i] = new Nullable!(Algebraic!cfloat)[];
-			if(is(columnTypes[i] == cdouble)) table[i] = new Nullable!(Algebraic!cdouble)[];
-			if(is(columnTypes[i] == creal)) table[i] = new Nullable!(Algebraic!creal)[];
-			if(is(columnTypes[i] == ubyte[])) table[i] = new Nullable!(Algebraic!ubyte[])[];
-			if(is(columnTypes[i] == string)) table[i] = new Nullable!(Algebraic!string)[];
-			if(is(columnTypes[i] == wstring)) table[i] = new Nullable!(Algebraic!wstring)[];
-			if(is(columnTypes[i] == dstring)) table[i] = new Nullable!(Algebraic!dstring)[];
-			if(is(columnTypes[i] == UUID)) table[i] = new Nullable!(Algebraic!UUID)[];
-			if(is(columnTypes[i] == Date)) table[i] = new Nullable!(Algebraic!Date)[];
-			if(is(columnTypes[i] == DateTime)) table[i] = new Nullable!(Algebraic!DateTime)[];
-			if(is(columnTypes[i] == TimeOfDay)) table[i] = new Nullable!(Algebraic!TimeOfDay)[];
-			if(is(columnTypes[i] == Duration)) table[i] = new Nullable!(Algebraic!Duration)[];
-			if(is(columnTypes[i] == SqlTable)) table[i] = new Nullable!(Algebraic!SqlTable)[];
-			if(is(columnTypes[i] == Object)) table[i] = new Nullable!(Algebraic!Object)[];
-		}
+		auto value = table[row][column];
+		if(value.isNull())
+			return Nullable!T();
+		return Nullable!T(value.get().get!T());
 	}
 
-	public this(T...)(uint reserveRows, T columnTypes)
-		if(allSatisfy!(columnTypes, bool, byte, ubyte, short, ushort, int, uint, long, ulong,// cent, ucent, -- Compiler error when included
-			float, double, real, ifloat, idouble, ireal, cfloat, cdouble, creal, char, wchar, dchar,
-			ubyte[], string, wstring, dstring, UUID, Date, DateTime, TimeOfDay, Duration, SqlTable, Object))
+	public void setField(T)(uint row, uint column, Nullable!T value)
 	{
-		table.reserve(columnTypes.length);
+		if(value is null)
+			table[row][column].nullify();
+		table[row][column] = Variant(value.get());
+	}
+}
 
-		for(int i = 0; i < columnTypes.length; i++)
-		{
-			if(is(columnTypes[i] == bool)) table[i] = new Nullable!(Algebraic!bool)[];
-			if(is(columnTypes[i] == byte)) table[i] = new Nullable!(Algebraic!byte)[];
-			if(is(columnTypes[i] == ubyte)) table[i] = new Nullable!(Algebraic!ubyte)[];
-			if(is(columnTypes[i] == short)) table[i] = new Nullable!(Algebraic!short)[];
-			if(is(columnTypes[i] == ushort)) table[i] = new Nullable!(Algebraic!ushort)[];
-			if(is(columnTypes[i] == int)) table[i] = new Nullable!(Algebraic!int)[];
-			if(is(columnTypes[i] == uint)) table[i] = new Nullable!(Algebraic!uint)[];
-			if(is(columnTypes[i] == long)) table[i] = new Nullable!(Algebraic!long)[];
-			if(is(columnTypes[i] == ulong)) table[i] = new Nullable!(Algebraic!ulong)[];
-			if(is(columnTypes[i] == float)) table[i] = new Nullable!(Algebraic!float)[];
-			if(is(columnTypes[i] == double)) table[i] = new Nullable!(Algebraic!double)[];
-			if(is(columnTypes[i] == real)) table[i] = new Nullable!(Algebraic!real)[];
-			if(is(columnTypes[i] == ifloat)) table[i] = new Nullable!(Algebraic!ifloat)[];
-			if(is(columnTypes[i] == idouble)) table[i] = new Nullable!(Algebraic!idouble)[];
-			if(is(columnTypes[i] == ireal)) table[i] = new Nullable!(Algebraic!ireal)[];
-			if(is(columnTypes[i] == cfloat)) table[i] = new Nullable!(Algebraic!cfloat)[];
-			if(is(columnTypes[i] == cdouble)) table[i] = new Nullable!(Algebraic!cdouble)[];
-			if(is(columnTypes[i] == creal)) table[i] = new Nullable!(Algebraic!creal)[];
-			if(is(columnTypes[i] == ubyte[])) table[i] = new Nullable!(Algebraic!ubyte[])[];
-			if(is(columnTypes[i] == string)) table[i] = new Nullable!(Algebraic!string)[];
-			if(is(columnTypes[i] == wstring)) table[i] = new Nullable!(Algebraic!wstring)[];
-			if(is(columnTypes[i] == dstring)) table[i] = new Nullable!(Algebraic!dstring)[];
-			if(is(columnTypes[i] == UUID)) table[i] = new Nullable!(Algebraic!UUID)[];
-			if(is(columnTypes[i] == Date)) table[i] = new Nullable!(Algebraic!Date)[];
-			if(is(columnTypes[i] == DateTime)) table[i] = new Nullable!(Algebraic!DateTime)[];
-			if(is(columnTypes[i] == TimeOfDay)) table[i] = new Nullable!(Algebraic!TimeOfDay)[];
-			if(is(columnTypes[i] == Duration)) table[i] = new Nullable!(Algebraic!Duration)[];
-			if(is(columnTypes[i] == Object)) table[i] = new Nullable!(Algebraic!Object)[];
+public final class SqlTable(T...) : SqlTableBase
+{
+	public this()
+	{
+		table = new Nullable!Variant[][](T.length, 0);
+	}
 
-			table[i].reserve(reserveRows);
-		}
+	public this(uint reserveRows)
+	{
+		table = new Nullable!Variant[][](T.length, reserveRows);
 	}
 }
 
 unittest
 {
-	SqlTable unreserved = new SqlTable!(string, wstring, bool, long)();
-	SqlTable reserved = new SqlTable!(string, wstring, bool, long)(100);
+	import std.stdio;
+
+	SqlTableBase unreserved = new SqlTable!(string, wstring, bool, long)();
+	SqlTableBase reserved = new SqlTable!(string, wstring, bool, long)(100);
+
+	reserved.setField!string(0,0,Nullable!string("Hello World!"));
+	auto test = reserved.getField!string(0,0);
+
+	writeln(test);
+	assert(test == "Hello World!");
 }
